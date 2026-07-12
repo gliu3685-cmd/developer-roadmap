7a2972d07a49b8af10ef505771766d0b55e4a2be
gliu3685-cmd <gliu3685@gmail.com>
2026-07-12 22:26:52 +0800

    Complete Linux Day4 shell scripting practice

--- linux/day04/log.sh
+++ linux/day04/log.sh
@@ -2,8 +2,12 @@
 
 DATE=$(date "+%Y-%m-%d")
 
-CONTENT=$1
+if [ -z "$1" ]
+then
+    echo "请输入学习内容"
+    exit 1
+fi
 
-echo "$DATE $CONTENT" >> study.log
+echo "$DATE $1" >> study.log
 
 echo "Log saved!"
